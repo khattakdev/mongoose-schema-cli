3a5bec5138f5a26e454e9a425cf2ed873349fe8a
Khattak <akkhattak65@gmail.com>
2020-08-14 12:59:18 +0500

    Fix Schema Generator Issue as npm package

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ const fs = require("fs");
 const { promisify } = require("util");
 const Listr = require("listr");
 const schema = require("../template");
+const chalk = require("chalk");
 
 const writeFile = promisify(fs.writeFile);
 const appendFile = promisify(fs.appendFile);
@@ -11,50 +12,27 @@ export async function createSchema(options, schemaKeyValues) {
   const schemaOptions = {
     ...options,
     dirPath: path.resolve(__dirname, "../template"),
-    outPath: path.resolve(__dirname, "../out", `./${options.schema}.js`),
+    outPath: path.resolve(process.cwd(), `./${options.schema}.js`),
   };
 
   console.log();
 
-  // initializeSchema(schemaOptions.outPath);
-  // await createObjects(schemaOptions.outPath, schemaKeyValues);
-  // exportSchema(schemaOptions.outPath, schema.schemaBottom());
-
   const tasks = new Listr([
     {
-      title: "Creating File and Adding Imports",
-      task: () => initializeSchema(schemaOptions.outPath),
-    },
-    {
-      title: "Adding Schema Keys",
+      title: "Creating Schema",
       task: async () =>
         await createObjects(schemaOptions.outPath, schemaKeyValues),
-    },
-    {
-      title: "Making Schema Exportable",
-      task: () => exportSchema(schemaOptions.outPath, schema.schemaBottom()),
     },
   ]);
 
   await tasks.run();
-
-  // Append Objects to the Schema
-
-  // Close the Schema and Add Export
-
-  // await appendFile(schemaOptions.out, schema.schemaBottom(true));
-}
-
-async function initializeSchema(outPath) {
-  await writeFile(outPath, schema.schemaTop());
+  console.log(chalk.green("DONE!"), "Schema Generated");
 }
 
 async function createObjects(outPath, schemaKeyValues) {
+  await writeFile(outPath, schema.schemaTop());
   for (let i = 0; i < schemaKeyValues.length; i++) {
     await appendFile(outPath, schema.createSchemaObject(schemaKeyValues[i]));
   }
-}
-
-async function exportSchema(outPath) {
   await appendFile(outPath, schema.schemaBottom());
 }

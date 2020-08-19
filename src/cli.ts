919b878720a632843b70276c60c50cb4dccaddec
Khattak <akkhattak65@gmail.com>
2020-08-19 15:35:16 +0500

    mend
    Move Prompt Functions to separate File

--- src/cli.ts
+++ src/cli.ts
@@ -1,6 +1,7 @@
 import arg from "arg";
 import inquirer from "inquirer";
 import createSchema from "./main";
+import { promptForMissingOptions, promptForSchemaObject } from "./prompts";
 import * as dataType from "./types";
 
 /**
@@ -55,95 +56,6 @@ function parseArgumentsIntoOptions(
   return {
     isTypescript: args["--typescript"] || false,
     filePath: args["--filepath"] || "/",
-  };
-}
-
-/**
- * Ask for schema related data e.g. Schema's Name
- * @param options Filtered Arguments provided by User
- */
-async function promptForMissingOptions(
-  options: dataType.OptionsType
-): Promise<{
-  isTypescript: boolean;
-  schema: string;
-}> {
-  const defaultOptions = {
-    schema: "default",
-  };
-
-  const questions: dataType.QuestionType[] = [];
-
-  questions.push({
-    type: "input",
-    name: "schema",
-    message: "Please input Schema name",
-    default: defaultOptions.schema,
-  });
-
-  const answers = await inquirer.prompt(questions);
-  return {
-    ...options,
-    schema: answers.schema,
-  };
-}
-
-/**
- * Take User Input for Schema's Key
- *
- * This include data such as
- * Schema Key's name, type, default Value etc
- */
-async function promptForSchemaObject() {
-  const defaultOptions = {
-    name: "default",
-    type: "String",
-    required: true,
-    default: "",
-  };
-  const questions: dataType.QuestionType[] = [];
-
-  questions.push({
-    type: "input",
-    name: "name",
-    message: "Please input Schema Key name",
-    default: defaultOptions.name,
-  });
-
-  questions.push({
-    type: "list",
-    name: "type",
-    message: "Please input Schema Key type",
-    choices: ["String", "Number", "Boolean"],
-    default: defaultOptions.name,
-  });
-
-  questions.push({
-    type: "confirm",
-    name: "required",
-    message: "Is this Schema Key Required",
-    default: defaultOptions.required,
-  });
-
-  questions.push({
-    type: "input",
-    name: "default",
-    message: "What is the default value?",
-    default: defaultOptions.default,
-  });
-
-  const answers: {
-    name: string;
-    type: string;
-    required: boolean;
-    default: string;
-  } = await inquirer.prompt(questions);
-
-  return {
-    name: answers.name,
-    type: answers.type,
-    isRequired: answers.required,
-    defaultValue: answers.default,
   };
 }
 
